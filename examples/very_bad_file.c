1ad2b95bc01ce1413e1a25e4ecbe214509804f31
Valentin Pelloin <valentinpelloin@gmail.com>
2017-05-01 15:37:19 +0200

    Initial commit

---
+++ examples/very_bad_file.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+
+// 
+// TYPES
+//
+
+/*
+Definition of a structure
+*/
+typedef struct 
+{
+int anInt; // an int inside the type
+float aFloat; // a float inside the type
+unsigned long  ( *  functionPointer ) ( const char  * ); // a function pointer
+}
+myType ;
+
+/*
+Definition of an enum
+*/
+typedef enum
+{
+        	 	  FALSE, TRUE
+} myBool
+;
+
+
+
+
+//
+// FUNCTIONS
+//
+
+// This function does something, but I don't know what :)
+int test_function (int a, int b)
+{
+    int i ;
+	int j = 10;
+
+	for (i = 0 ; i < a ; i++){
+		// A comment
+		if(i == b)
+			return i ;
+		else
+			j++;
+	}
+
+	// HELLO
+	while(a != b && a < b){
+		a += 1;
+	}
+
+	return (a) ;
+}
+
+
+
+
+//
+// MAIN FUNCTION
+//
+
+int
+main()
+{
+ int a, b, c, d, e, f, g;
+ float h = 0, i = 2, j;
+ char * str = "Hello, how are you?" ;
+ int str_len = strlen( str ) ;
+ float bar = 2.0
+		 ;
+ int value = (int) bar;
+ myType variable1 = { 5 , 3.0 , strlen } ;
+ myBool variable2 = FALSE ;
+
+
+
+ /* SWITCH */
+ switch(value){
+	case 1: return 0;
+	case 2: return 1;
+	case 3: return 2;
+	
+	default: printf("Nothing to do");
+ } 
+
+
+
+ printf("%i", test_function(10, 20)) ;
+
+ printf( "%s - %i - %f\n" , str , str_len , bar ) ;
+ printf( "{%i - %f} - %i\n" , variable1.anInt, variable1.aFloat, variable2) ;
+
+
+
+
+}
---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+echo -e "\n\n"
+
+echo -e "\t############################"
+echo -e "\t# CLANG-FORMAT AUTO CONFIG #"
+echo -e "\t############################\n\n"
+
+
+if (test $# -gt 1) then
+	echo "Usage: $0 [TRUE/FALSE : override the backup file]"
+	echo -e "\n	Warning: This program will remove all the clang-format files."
+	echo "	A backup of you old files will be created : ~/.clang-format.backup"
+else
+
+if (test -f ~/.clang-format.backup -a "$1" != "TRUE") then
+	echo "A backup of your clang-format files already exists, please remove it:"
+	echo "		rm ~/.clang-format.backup"
+	echo "or override it with:"
+	echo "		$0 TRUE"
+else
+
+	# BACKUP
+
+	echo "<- Creating a backup of the old files ->"
+	echo -n "   ..."
+
+	if (test -f ~/.clang-format) then
+
+		if(test -f ~/.clang-format.backup) then
+			rm ~/.clang-format.backup
+		fi
+		mv ~/.clang-format ~/.clang-format.backup
+
+
+	else
+		echo -n "   ... No backup to do"
+	fi
+	
+	echo " ... Done"
+	
+	# LINK
+
+	echo "<- Creating link to the new file ->"
+	echo -n "   ..."
+
+	ln -s ~/git/indentator/.clang-format ~/.clang-format
+
+	echo " ... Done"
+
+
+	echo "Your clang-format config file is now ready ! :)"
+
+fi
+fi
+
+echo -e "\n\n"
